2c3fae00a7a6dc9e97e26683e28b56731e3d1cba
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-05 23:33:42 +0200

    Added useState and useNavigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css';
 import Home from './Components/Home/Home';
 import Recipes from './Components/Recipes/Recipe';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Recipes />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search/:query" element={<Recipes />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../Home/Home.css";
 
 const Home = () => {
+    // State
+    const [query, setQuery] = useState("");
+    const navigate = useNavigate();
+
+    // save changes
+    const handleQuery = (e) => {
+        const value = e.target.value;
+        setQuery(value);
+    }
+
+    // move to page with query value url 
+    const handleSearch = (e) => {
+        e.preventDefault();
+        navigate(`/search/${query}`);
+    }
+
     return (
         <div className="home">
             <div className="search">
@@ -11,8 +28,8 @@ const Home = () => {
                         <small className="small-text">Find the recipes you love best</small>
                     </div>
                     <form>
-                        <input type="text" />
-                        <button type="submit">Search</button>
+                        <input type="text" value={query} onChange={handleQuery}/>
+                        <button onClick={handleSearch}>Search</button>
                     </form>
                 </div>
             </div>
