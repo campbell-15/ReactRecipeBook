b276f22c1ce0e8ed6ee1481c9387d035ea7183ca
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-05 20:18:11 +0200

    added home.js component

---
+++ src/Components/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="search">
+                <div className="inputs">
+                    <div className="logo">
+                        <h2>View Our React Recipes</h2>
+                        <small>Find the recipes you love best</small>
+                    </div>
+                    <form>
+                        <input type="text" />
+                        <button type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
