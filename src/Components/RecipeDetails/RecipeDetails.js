eb54292b502a01cd25ae84b7ed2d9e5ee50316cc
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-04-09 15:45:43 +0200

    test styling for deployment

--- src/Components/RecipeDetails/RecipeDetails.js
+++ src/Components/RecipeDetails/RecipeDetails.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
-// import './RecipeDetails.css'
+import './RecipeDetails.css'
 
 function RecipeDetails() {
     // Access the id parameter from the URL
@@ -69,10 +69,10 @@ function RecipeDetails() {
     return (
         <div>
             <div className="button-container">           
-                <Link to="/" className="button">Back</Link>
+                <Link to="/" className="back-button">Back</Link>
             </div>
             <div className="deatils-card">
-                <h1>{recipe.title}</h1>
+                <h1 className="ingredients-title">{recipe.title}</h1>
                 <p>{recipe.description}</p>
                 <h2>Ingredients:</h2>
                 <ul>
